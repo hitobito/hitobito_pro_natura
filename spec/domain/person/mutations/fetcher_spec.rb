c946a3691a0055cf4f14d9289997a9073dc405c0
Daniel Illi <daniel@illi.zone>
2023-10-30 16:03:30 +0100

    Fix specs

--- spec/domain/person/mutations/fetcher_spec.rb
+++ spec/domain/person/mutations/fetcher_spec.rb
@@ -135,8 +135,8 @@ describe Person::Mutations::Fetcher, versioning: true do
       role = @role_deleted.roles.with_deleted.find { |r| r.group_id == groups(:thun).id }
       expect(modification.changed_at).to be_within(0.01).of(role.deleted_at)
       expect(modification.kind).to eq(:updated)
-      expect(modification.changeset.keys).to have(8).items
-      #binding.pry
+      expect(modification.changeset.keys).to match_array %w(id person_id group_id type
+                                                            label created_at deleted_at archived_at delete_on)
       expect(modification.role_changes).to eq(true)
     end
 
