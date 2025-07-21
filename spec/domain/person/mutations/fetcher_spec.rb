2e46bcb9510af4a8bc6c2c77800485aeb6d9b5b4
Philipp <8806985+philippks@users.noreply.github.com>
2025-07-21 13:20:44 +0200

    Include all changes on leaders in mutation export (#15)  (#23)
    * Revert "Revert mutations change"
    
    This reverts commit 0bc88f57a29dad5b1823b63b8cf6f79086397d9c.
    
    * Only export leaders in mutation export (hitobito/hitobito_pro_natura#15)
    
    * Fix time-based test
    
    * Fix time-based test
    
    ---------
    
    Co-authored-by: Philipp Koster <koster@puzzle.ch>
    Co-authored-by: Carlo Beltrame <beltrame@puzzle.ch>

--- spec/domain/person/mutations/fetcher_spec.rb
+++ spec/domain/person/mutations/fetcher_spec.rb
@@ -8,15 +8,15 @@ require "spec_helper"
 describe Person::Mutations::Fetcher, versioning: true do
   before do
     @created = Fabricate(:person, first_name: "@created").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun))
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun))
     end
     @updated = Fabricate(:person, first_name: "@updated").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun))
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun))
     end
     @updated.update_column(:created_at, 1.year.ago)
     @multi_roles = Fabricate(:person, first_name: "@multi_roles").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.months.ago, end_on: 1.month.ago)
-      Fabricate(Group::Sektion::Admin.name, person: _1, group: groups(:be))
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun), start_on: 2.months.ago, end_on: 1.month.ago)
+      Fabricate(Group::DachverbandGremium::Leader.name, person: _1, group: groups(:gs))
     end
 
     @before = create_past(first_name: "@before")
@@ -25,39 +25,35 @@ describe Person::Mutations::Fetcher, versioning: true do
     @role_added = create_past(first_name: "@role_added").tap do
       Fabricate(Group::Sektion::Admin.name, group: groups(:be), person: _1)
     end
-    @role_deleted = create_past([Group::Jugendgruppe::Member, groups(:thun)], [Group::Sektion::Admin, groups(:be)],
+    @role_deleted = create_past([Group::Jugendgruppe::Leader, groups(:thun)], [Group::DachverbandGremium::Leader, groups(:gs)],
       first_name: "@role_deleted").tap do
       _1.roles.where(group: groups(:thun)).each {|r| r.update!(end_on: Date.current.yesterday) }
     end
-    @primary_group_changed = create_past([Group::Jugendgruppe::Member, groups(:thun)], [Group::Sektion::Admin, groups(:be)],
+    @primary_group_changed = create_past([Group::Jugendgruppe::Leader, groups(:thun)], [Group::DachverbandGremium::Leader, groups(:gs)],
                                          first_name: "@primary_group_changed").tap do
       _1.update!(primary_group_id: groups(:be).id)
     end
     @deleted = Fabricate(:person, first_name: "@deleted").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.months.ago, end_on: 1.month.ago)
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun), start_on: 2.months.ago, end_on: 1.month.ago)
     end
     @deleted_longtime = Fabricate(:person, first_name: "@deleted_longtime").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
     end
     @deleted_multi = Fabricate(:person, first_name: "@deleted_multi").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
-      Fabricate(Group::Sektion::Admin.name, person: _1, group: groups(:be), start_on: 2.months.ago, end_on: 1.month.ago)
+      Fabricate(Group::Jugendgruppe::Leader.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
+      Fabricate(Group::DachverbandGremium::Leader.name, person: _1, group: groups(:gs), start_on: 2.months.ago, end_on: 1.month.ago)
+    end
+    @mutation_after_role_is_deleted = create_past([Group::Jugendgruppe::Leader, groups(:thun)], first_name: "@mutation_after_role_is_deleted").tap do
+      _1.roles.where(group: groups(:thun)).each {|r| r.update!(end_on: 1.week.ago) }
+      _1.phone_numbers.create!(number: "+41790000000", label: "Privat")
     end
     @passive = Fabricate(:person, first_name: "@passive").tap do
       Fabricate(Group::JugendgruppePassive::Member.name, person: _1, group: groups(:thun_passive))
     end
-    @passive_deleted = Fabricate(:person, first_name: "@passive_deleted").tap do
-      Fabricate(Group::JugendgruppePassive::Member.name, person: _1, group: groups(:thun_passive))
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
-    end
-    @passive_deleted_recently = Fabricate(:person, first_name: "@passive_deleted_recently").tap do
-      Fabricate(Group::Jugendgruppe::Member.name, person: _1, group: groups(:thun), start_on: 2.years.ago, end_on: 1.year.ago)
-      Fabricate(Group::JugendgruppePassive::Member.name, person: _1, group: groups(:thun_passive), start_on: 2.months.ago, end_on: 1.month.ago)
-    end
   end
 
   def create_past(*roles, **person_attrs)
-    roles = [[Group::Jugendgruppe::Member, groups(:thun)]] if roles.blank?
+    roles = [[Group::Jugendgruppe::Leader, groups(:thun)]] if roles.blank?
 
     Fabricate(:person, **person_attrs.reverse_merge(created_at: 1.year.ago, updated_at: 1.year.ago)).tap do |person|
       roles.each do |role, group|
@@ -70,34 +66,12 @@ describe Person::Mutations::Fetcher, versioning: true do
 
   let(:fetcher) { Person::Mutations::Fetcher.new(3.months.ago) }
 
-  context "#mutated_people" do
-    subject { fetcher.mutated_people.collect(&:to_s) }
+  context "#mutations" do
+    subject { fetcher.mutations }
 
-    it "contains only changed people" do
-      is_expected.to match_array([@created,
-        @updated,
-        @multi_roles,
-        @phone_changed,
-        @role_added,
-        @role_deleted,
-        @primary_group_changed].collect(&:to_s))
-    end
-  end
-
-  context "#deleted_people" do
-    subject { fetcher.deleted_people.collect(&:to_s) }
-
-    it "contains only deleted" do
-      is_expected.to match_array([@deleted,
-        @deleted_multi].collect(&:to_s))
-    end
-  end
-
-  context "#fetch" do
-    subject { fetcher.fetch }
-
-    it "contains all people" do
-      expect(subject.collect(&:to_s)).to match_array([@deleted,
+    it "contains all relevant changes" do
+      expected = [
+        @deleted,
         @deleted_multi,
         @created,
         @updated,
@@ -105,7 +79,21 @@ describe Person::Mutations::Fetcher, versioning: true do
         @phone_changed,
         @role_added,
         @role_deleted,
-        @primary_group_changed].collect(&:to_s))
+        @primary_group_changed,
+        @mutation_after_role_is_deleted
+      ].map(&:to_s)
+
+      expect(subject.map(&:to_s)).to include(*expected)
+    end
+
+    it "contains multiple entries when multiple changes were applied" do
+      expect(subject.map(&:to_s).count(@updated.to_s)).to eq 2
+      expect(subject.map(&:to_s).count(@multi_roles.to_s)).to eq 3
+      expect(subject.map(&:to_s).count(@deleted.to_s)).to eq 3
+    end
+
+    it "does not contain changes on people with passive roles" do
+      expect(subject.map(&:to_s)).not_to include(@passive.to_s)
     end
 
     it "contains changeset for update" do
@@ -113,7 +101,7 @@ describe Person::Mutations::Fetcher, versioning: true do
       expect(modification.changed_at).to be_within(1).of(@primary_group_changed.updated_at)
       expect(modification.kind).to eq(:updated)
       expect(modification.changeset).to eq("primary_group_id" => [groups(:thun).id, groups(:be).id])
-      expect(modification.role_changes).to eq(false)
+      expect(modification.role_changes).to be_falsey
     end
 
     it "contains changeset for create" do
@@ -121,16 +109,17 @@ describe Person::Mutations::Fetcher, versioning: true do
       expect(modification.changed_at).to be_within(1).of(@created.created_at)
       expect(modification.kind).to eq(:created)
       expect(modification.changeset).to be_present
-      expect(modification.role_changes).to eq(true)
+      expect(modification.role_changes).to be_falsey
     end
 
-    it "contains no changeset for delete" do
+    it "contains changeset for delete" do
       modification = subject.find { |m| m.id == @deleted_multi.id }
-      role = @deleted_multi.roles.with_inactive.to_a.find { |r| r.group_id == groups(:be).id }
+      role = @deleted_multi.roles.with_inactive.to_a.find { |r| r.group_id == groups(:gs).id }
       expect(modification.changed_at).to eq role.end_on.beginning_of_day
       expect(modification.kind).to eq(:deleted)
       expect(modification.changeset).to eq({})
-      expect(modification.role_changes).to eq(nil)
+      expect(modification.role_changes).to be_falsey
+      expect(modification.primary_roles.first).to start_with "Leiter/in (bis"
     end
 
     it "contains changeset for phone number" do
@@ -147,7 +136,7 @@ describe Person::Mutations::Fetcher, versioning: true do
       expect(modification.changed_at).to be_within(0.01).of(role.created_at)
       expect(modification.kind).to eq(:updated)
       expect(modification.changeset.keys).to have(6).items
-      expect(modification.role_changes).to eq(true)
+      expect(modification.role_changes).to be_truthy
     end
 
     it "contains changeset for role deleted" do
@@ -156,7 +145,13 @@ describe Person::Mutations::Fetcher, versioning: true do
       expect(modification.changed_at).to eq role.updated_at
       expect(modification.kind).to eq(:updated)
       expect(modification.changeset).to eq("end_on" => [nil, role.end_on])
-      expect(modification.role_changes).to eq(true)
+      expect(modification.role_changes).to be_truthy
+    end
+
+    it "contains changeset for changes, even when role was deleted" do
+      modification = subject.find { |m| m.id == @mutation_after_role_is_deleted.id && m.changeset.keys.include?("number") }
+      expect(modification.kind).to eq(:updated)
+      expect(modification.primary_roles).to eq ["Leiter/in (bis #{1.week.ago.strftime('%d.%m.%Y')})"]
     end
   end
 end
